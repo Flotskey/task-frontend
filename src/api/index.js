a37939833394ab43f37543794b6735ceb5bab7a8
Flotskey <flotskey@gmail.com>
2022-04-24 04:27:36 +0300

    update baseURL

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 import { buildings } from "./buildings";
 
-const baseURL = "http://localhost:5065/api/";
+const baseURL =
+  process.env.NODE_ENV !== "production"
+    ? "http://localhost:5065/api/"
+    : "https://task-backend.ddns.net:7065/api";
+
 export const axiosInstance = axios.create({ baseURL });
 
 export const requests = {
